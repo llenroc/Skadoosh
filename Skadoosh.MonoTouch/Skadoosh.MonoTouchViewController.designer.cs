112b748938640ee46c8525ef288ef09d6b4c578e
developer ios <quicksilver_65@hotmail.com>
2013-10-08 15:13:48 -0700

    fixed

--- Skadoosh.MonoTouch/Skadoosh.MonoTouchViewController.designer.cs
+++ Skadoosh.MonoTouch/Skadoosh.MonoTouchViewController.designer.cs
@@ -1,17 +1,34 @@
+// WARNING
 //
-// This file has been generated automatically by MonoDevelop to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
+// This file has been generated automatically by Xamarin Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
 // Manual changes to this file may not be handled correctly.
 //
 using MonoTouch.Foundation;
+using System.CodeDom.Compiler;
 
 namespace Skadoosh.MonoTouch
 {
 	[Register ("Skadoosh_MonoTouchViewController")]
 	partial class Skadoosh_MonoTouchViewController
 	{
+		[Outlet]
+		MonoTouch.UIKit.UIButton btnLive { get; set; }
+
+		[Outlet]
+		MonoTouch.UIKit.UIButton btnStatic { get; set; }
+		
 		void ReleaseDesignerOutlets ()
 		{
+			if (btnLive != null) {
+				btnLive.Dispose ();
+				btnLive = null;
+			}
+
+			if (btnStatic != null) {
+				btnStatic.Dispose ();
+				btnStatic = null;
+			}
 		}
 	}
 }
